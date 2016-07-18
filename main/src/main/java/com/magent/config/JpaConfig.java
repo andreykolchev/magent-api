1627cf3e0a5630f6422d79406f773f0faeacf0fd
artomov.ihor <artomov.i.h@roots.com.ua>
2016-07-18 19:46:43 +0300

    SAP_182 finish rename all id according naming conventions

--- main/src/main/java/com/magent/config/JpaConfig.java
+++ main/src/main/java/com/magent/config/JpaConfig.java
@@ -22,7 +22,6 @@ import java.util.Properties;
  */
 @Configuration
 @EnableTransactionManagement
-@ImportResource({"classpath:dbinicialize.xml"})
 @ComponentScan({"com.magent.repository", "com.magent.service, com.magent.domain"})
 @EnableJpaRepositories(value = "com.magent.repository", repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
 @PropertySources({
