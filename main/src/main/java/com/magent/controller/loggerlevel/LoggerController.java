73b4dba9df0b111486f5aa76c91036be1ff9839f
artomov.ihor <artomov.i.h@roots.com.ua>
2016-07-27 17:41:38 +0300

    SAP_200 add 409 status for ConstraintViolationException

--- main/src/main/java/com/magent/controller/loggerlevel/LoggerController.java
+++ main/src/main/java/com/magent/controller/loggerlevel/LoggerController.java
@@ -5,6 +5,7 @@ import com.magent.utils.validators.OnBoardingValidatorImpl;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import javassist.NotFoundException;
 import org.apache.log4j.Logger;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.RollbackException;
+import javax.validation.ConstraintViolationException;
 import javax.xml.bind.ValidationException;
 import java.io.IOException;
 import java.text.ParseException;
@@ -27,42 +29,35 @@ import java.util.Arrays;
 public class LoggerController {
     private final static Logger LOGGER = org.apache.log4j.Logger.getLogger(LoggerController.class);
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler({NullPointerException.class,
-            RollbackException.class, javax.persistence.RollbackException.class, IllegalArgumentException.class})
+            RollbackException.class, javax.persistence.RollbackException.class, IllegalArgumentException.class, DataIntegrityViolationException.class, ConstraintViolationException.class})
     public void nullPointerException(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
-        LOGGER.warn("WARNING exception: " + request.getRequestURI() + " with exception " + e+" "+ Arrays.toString(e.getStackTrace()));
-        notFoundDefault(request, response, e);
+        LOGGER.warn("WARNING exception: " + request.getRequestURI() + " with exception " + e + " " + Arrays.toString(e.getStackTrace()));
+        exceptionWriter(request, response, e);
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({NotFoundException.class})
     public void notFoundException(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
-        notFoundDefault(request, response, e);
-    }
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({IOException.class})
-    public void IOException(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
-        notFoundDefault(request, response, e);
-        LOGGER.warn("WARNING IOException: " + request.getRequestURI() + " with exception " + e+" "+ Arrays.toString(e.getStackTrace()));
-    }
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({ParseException.class})
-    public void parseException(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
-        notFoundDefault(request, response, e);
-        LOGGER.warn("WARNING ParseException: " + request.getRequestURI() + " with exception " + e+" "+ Arrays.toString(e.getStackTrace()));
-    }
-
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler({Exception.class})
-    public void exception(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
-        LOGGER.warn("WARNING NOT KNOWABLE EXCEPTION: " + request.getRequestURI() + " with exception " + e+" "+ Arrays.toString(e.getStackTrace()));
+        exceptionWriter(request, response, e);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({OnBoardingValidatorImpl.InvalidOnboardEntity.class, ImageValidatorImpl.NotCorrectImageExtension.class, ValidationException.class,javax.xml.bind.ValidationException.class,})
+    @ExceptionHandler({IOException.class, ParseException.class})
+    public void IOException(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
+        exceptionWriter(request, response, e);
+        LOGGER.warn("WARNING IOException: " + request.getRequestURI() + " with exception " + e + " " + Arrays.toString(e.getStackTrace()));
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler({Exception.class})
+    public void exception(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
+        LOGGER.warn("WARNING NOT KNOWABLE EXCEPTION: " + request.getRequestURI() + " with exception " + e + " " + Arrays.toString(e.getStackTrace()));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({OnBoardingValidatorImpl.InvalidOnboardEntity.class, ImageValidatorImpl.NotCorrectImageExtension.class, ValidationException.class, javax.xml.bind.ValidationException.class,})
     public void badOnboardEntity(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
         response.getWriter().println("bad request : " + e.getMessage());
         LOGGER.info("bad request : " + request.getRequestURI() + " with exception " + e);
@@ -79,8 +74,8 @@ public class LoggerController {
     }
 
     @SuppressFBWarnings("CRLF_INJECTION_LOGS")
-    private void notFoundDefault(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
+    private void exceptionWriter(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
         response.getWriter().println("message " + e.getMessage());
-        LOGGER.info("not found exception in: " + request.getRequestURI() + " with exception " + e);
+        LOGGER.info("exception in : " + request.getRequestURI() + " with exception " + e);
     }
 }
